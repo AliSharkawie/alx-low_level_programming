5e063154c3360519db45c1d4244cfa165bc959c4
AliSharkawie <11410120200334@stud.cu.edu.eg>
2023-07-16 14:46:54 -0700

    mm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Description: This is the main function that prints a message.
+ * Description: This program prints a message to the standard error.
  *
- * Return: Always 0 (Success)
+ * Return: Always 1
 */
 
 int main(void)
 {
-	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, x, 59);
 	return (1);
 }
