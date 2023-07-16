4bf961984a419d8e8a983d23af858f1fdba61cdf
AliSharkawie <11410120200334@stud.cu.edu.eg>
2023-07-16 14:54:23 -0700

    mm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,8 @@
 
 int main(void)
 {
-	char x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char x = "and that piece of art is usefu
+l\" - Dora Korpar, 2015-10-19";
 
 	write(1, x, 59);
 	return (1);
