74b1d57de2760b318df493715bc245a130f18eba
AliSharkawie <11410120200334@stud.cu.edu.eg>
2023-07-18 13:32:48 -0700

    asd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+#include<math.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("%s", "_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readme for 0x02 functoin and nested loop c 
