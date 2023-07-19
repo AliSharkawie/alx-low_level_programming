277a9e7eb129ebe6da2cbcc971a2ab4974d5452a
AliSharkawie <11410120200334@stud.cu.edu.eg>
2023-07-19 13:48:46 -0700

    mm

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,8 +14,8 @@ int print_last_digit(int x)
 {
 	if (x < 0)
 	{
-		printf("%d", -(x % 10));
-		return (-(x % 10));
+		printf("%d", (-1 * (x % 10)));
+		return (-1 * (x % 10));
 	}
 	printf("%d", (x % 10));
 	return (x % 10);
