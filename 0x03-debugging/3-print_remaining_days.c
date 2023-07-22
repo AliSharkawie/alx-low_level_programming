6db20b365b7bbe1deca225e6b2bee334456dc8af
AliSharkawie <11410120200334@stud.cu.edu.eg>
2023-07-21 17:22:42 -0700

    mm

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) || ((year % 400 == 0) && (year % 100 == 0)))
+	/*
+	 * leap year when it devide by
+	 * 100 and 400
+	 *
+	*/
+
+	if ((year % 4 == 0) || ((year % 400 == 0) && !(year % 100 == 0)))
 	{
 		if (month >= 2 && day >= 60)
 		{
